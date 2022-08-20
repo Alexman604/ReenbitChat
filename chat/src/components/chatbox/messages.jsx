584cae1b5344ed7dc206c0aef04795a9dfc994c6
Alexman604 <goa-man-604@ukr.net>
2022-08-20 10:58:26 +0300

    search, date format, order in main left

--- chat/src/components/chatbox/messages.jsx
+++ chat/src/components/chatbox/messages.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import "./messages.css";
 
-const Messages = ({ ...props }) => 
-    <>
-      {props.messages.map((item, index) => (
-        <div className="message" key={index}>
-          
-            <img src={props.img} alt="" />
-            <div className="textContainer">
-              <span className="messageText"> {item.msg}</span>
-            <br />
-          </div> 
-          <br />
-              <span className="MessageDate"> {item.date} </span>
-            
+const Messages = ({ ...props }) => (
+  <>
+    {props.messages.map((item, index) => (
+      <div className="message" key={index}>
+        <div className="textContainer">
+          <img src={props.img} alt="" />
+          <span className="messageText"> {item.msg}</span>
         </div>
-      ))}
-    </>
-
+        <span className="MessageDate"> {item.date} </span>
+      </div>
+    ))}
+  </>
+);
 
 export default Messages;
